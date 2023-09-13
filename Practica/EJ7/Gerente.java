0a3a7e4409b695d57a9857901d6bb093c45da90f
milena confeggi <128049813+MilenaConfeggi@users.noreply.github.com>
2023-09-13 14:31:32 -0300

    Create Gerente.java

---
+++ Practica/EJ7/Gerente.java
@@ -0,0 +1,12 @@
+package ar.edu.unlp.info.oo1.objetos_uno.ejercicio7;
+
+public class Gerente {
+	
+	public double aportes() {
+		return (this.montoBasico() * 0.05);
+	}
+	
+	public double montoBasico() {
+		return 57000;
+	}
+}
