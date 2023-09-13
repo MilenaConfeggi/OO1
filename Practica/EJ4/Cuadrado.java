0556e354750d19c804762625f52b3bc6a3f0356d
milena confeggi <128049813+MilenaConfeggi@users.noreply.github.com>
2023-09-13 14:19:32 -0300

    Create Cuadrado.java

---
+++ Practica/EJ4/Cuadrado.java
@@ -0,0 +1,31 @@
+package ar.edu.unlp.info.oo1.objetos_uno.ejercicio4;
+
+public class Cuadrado extends forma{
+	private double lado;
+
+	public Cuadrado() {
+		
+	}
+
+	public Cuadrado( double lado) {
+		this.lado = lado;
+	}
+
+	public double getPerimetro() {
+		return lado * 4;
+	}
+	
+	public double getArea() {
+		return lado * lado;
+	}
+	
+	public double getLado() {
+		return lado;
+	}
+
+	public void setLado(double lado) {
+		this.lado = lado;
+	}
+
+	
+}
