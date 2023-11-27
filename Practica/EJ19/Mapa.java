407cdad85c6703f1c8d33dcf56e52b03bf875ab0
milena confeggi <128049813+MilenaConfeggi@users.noreply.github.com>
2023-11-27 15:53:19 -0300

    Create Mapa.java

---
+++ Practica/EJ19/Mapa.java
@@ -0,0 +1,19 @@
+package ar.edu.unlp.info.oo1.objetos_uno.ejercicio19;
+
+public class Mapa {
+	private String direc1;
+	private String direc2;
+	
+	
+	public Mapa(String direc1, String direc2) {
+		this.direc1 = direc1;
+		this.direc2 = direc2;
+	}
+
+
+	public int distanciaEntre() {
+		return 100;
+	}
+	
+	
+}
