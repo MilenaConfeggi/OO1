e9ed2bb94db03ffd179c535c04ac6e1a8cb2126e
milena confeggi <128049813+MilenaConfeggi@users.noreply.github.com>
2023-11-27 15:51:29 -0300

    Create Retirarcomercio.java

---
+++ Practica/EJ19/Retirarcomercio.java
@@ -0,0 +1,7 @@
+package ar.edu.unlp.info.oo1.objetos_uno.ejercicio19;
+
+public class retirarComercio implements Envio{
+	public double costoEnvio(String direc1, String direc2) {
+		return 0;
+	}
+}
