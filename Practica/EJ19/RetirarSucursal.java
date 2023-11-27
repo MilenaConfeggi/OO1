43881a6748aa2b93447c7ed201cbd0644eb1cfae
milena confeggi <128049813+MilenaConfeggi@users.noreply.github.com>
2023-11-27 15:51:05 -0300

    Create RetirarSucursal.java

---
+++ Practica/EJ19/RetirarSucursal.java
@@ -0,0 +1,7 @@
+package ar.edu.unlp.info.oo1.objetos_uno.ejercicio19;
+
+public class retirarSucursal implements Envio{
+	public double costoEnvio(String direc1, String direc2) {
+		return 50;
+	}
+}
